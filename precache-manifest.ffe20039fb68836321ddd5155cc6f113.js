f3d518d58970e8253008bc670ad64c1e147ade7d
n-westman <niklas@Niklass-MacBook-Pro.local>
2019-10-23 21:05:02 +0200

    Updates

---
+++ precache-manifest.ffe20039fb68836321ddd5155cc6f113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b47dc75051a73b79444081a0d5096c53",
+    "url": "./static/media/logo.b47dc750.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f8bad692406eeaa31817",
+    "url": "./static/js/main.c4f99484.chunk.js"
+  },
+  {
+    "revision": "41e334c6ed0455dbccb6",
+    "url": "./static/js/2.60205b95.chunk.js"
+  },
+  {
+    "revision": "f8bad692406eeaa31817",
+    "url": "./static/css/main.4c7747f1.chunk.css"
+  },
+  {
+    "revision": "77e0de21a2ae26296d6fb2c08460a0d7",
+    "url": "./index.html"
+  }
+];
